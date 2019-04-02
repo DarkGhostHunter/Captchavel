e06360189c1eb3b2a02367f5a69c44a69fef2c90
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2019-04-02 01:43:44 -0300

    Better phrasing for the Invalid Recaptcha Exception [skip ci]

--- src/Exceptions/InvalidRecaptchaException.php
+++ src/Exceptions/InvalidRecaptchaException.php
@@ -3,8 +3,18 @@
 namespace DarkGhostHunter\Captchavel\Exceptions;
 
 use Exception;
+use Throwable;
 
 class InvalidRecaptchaException extends Exception
 {
-    protected $message = 'The reCAPTCHA token received is invalid';
+    protected $message = 'The reCAPTCHA token is empty';
+
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        if (request()->input('_recaptcha') !== null) {
+            $this->message = 'The reCAPTCHA token received is invalid';
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
 }
