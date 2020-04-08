c8c30b678e787ec827653ad7756f79d8cc262952
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-04-08 13:32:58 -0400

    Reworked script to renew recaptcha script every 120 seconds.

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -9,24 +9,35 @@
             return;
         }
 
-        Array.from(document.getElementsByTagName('form'))
-            .filter((form) => form.dataset.recaptcha === 'true')
-            .forEach((form) => {
-                let action = form.action.includes('://') ? (new URL(form.action)).pathname : form.action;
-                grecaptcha.execute(site_key, {
-                    action: action
-                            .substring(action.indexOf('?'), action.length)
-                            .replace(/[^A-z\/_]/gi, '')
-                }).then((token) => {
-                    if (token) {
-                        let child = document.createElement('input');
-                        child.setAttribute('type', 'hidden');
-                        child.setAttribute('name', '_recaptcha');
-                        child.setAttribute('value', token);
-                        form.appendChild(child);
+        let elements = Array.from(document.getElementsByTagName('form'))
+            .filter((form) => form.dataset.recaptcha === 'true');
+
+        let renew = (form) => {
+            let action = form.action.includes('://') ? (new URL(form.action)).pathname : form.action;
+            grecaptcha.execute(site_key, {
+                action: action
+                    .substring(action.indexOf('?'), action.length)
+                    .replace(/[^A-z\/_]/gi, '')
+            }).then((token) => {
+                if (token) {
+
+                    let inputs = form.getElementsByClassName('recaptcha-token');
+
+                    if (inputs.length) {
+                        Array.from(inputs).forEach((input) => input.remove());
                     }
-                });
+
+                    let child = document.createElement('input');
+                    child.setAttribute('type', 'hidden');
+                    child.setAttribute('name', '_recaptcha');
+                    child.setAttribute('class', 'recaptcha-token');
+                    child.setAttribute('value', token);
+                    form.appendChild(child);
+                }
             });
+        };
+
+        setTimeout(() => elements.forEach(renew), 1000 * 120);
     };
     // End Captchavel Script
 </script>
